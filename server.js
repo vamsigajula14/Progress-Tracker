2968f380528b3cae4eac6bd3afb4deec0c536428
Vamsi Gajula <gajulavamsi90@gmail.com>
2025-09-23 23:52:14 +0530

    added cors

--- server.js
+++ server.js
@@ -1,13 +1,15 @@
 const express = require('express');
 require('dotenv').config();
 const connectDb = require('./config/db');
+const cors = require('cors');
 
 const PORT = process.env.PORT;
 
-const app = express()
+const app = express();
+app.use(cors());
 connectDb();
 
-app.use(express.json())
+app.use(express.json());
 
 app.use("/api/auth", require("./routes/authRoutes"));
 app.use("/api/users", require("./routes/userRoutes"));
